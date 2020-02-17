9d362e47718e0fc6917fd494f20013b28a6c1950
Joshua Fredrickson (RIT Student) <jjf4304@ad.rit.edu>
2020-02-17 10:49:34 -0500

    Start of Flick

---
+++ Golf-Assassin/Assets/Scripts/flickerControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flickerControls : MonoBehaviour
+{
+    public float powerMod;
+
+
+    private Touch touchCtrl;
+    private bool grounded;
+    private Rigidbody rgbd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody>();
+        grounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touchCtrl = Input.GetTouch(0);
+            Vector2 start, flickDirection;
+            if (touchCtrl.phase == TouchPhase.Began && grounded)
+            {
+                //detected touch.
+                start = touchCtrl.position;
+                Debug.Log("Touched");
+            }
+
+            if(touchCtrl.phase == TouchPhase.Moved)
+            {
+
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("GroundSurface"))
+        {
+            grounded = true;
+        }
+    }
+}
