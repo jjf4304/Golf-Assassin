d777d0046c4dee433ea90652f1951c8dfff5476b
jjf4304 <54686120+jjf4304@users.noreply.github.com>
2020-02-20 23:49:23 -0500

    New Version of Flick

---
+++ Golf-Assassin/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    Rigidbody myBody;
+    Rigidbody cameraBody;
+
+    public float cameraDistance = 10;
+    public float followPercentage = .2f;
+    public float minDistance = 3;
+    Vector3 lerpPosition = Vector3.zero;
+
+    float currentDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Save our rigidbody so we can use it later.
+        myBody = GetComponent<Rigidbody>();
+      
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Camera.main.transform.Rotate(Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up));
+        //Camera.main.transform.LookAt(transform.position);
+
+        Vector3 direction = transform.position - Camera.main.transform.position;
+        Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.up);
+        Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, newRotation, .05f);
+
+        Vector3 inverseVeloc = myBody.velocity.normalized * -1;
+
+        //Reduce the effect of y in moving the camera.
+        //inverseVeloc.y *= .2f;
+
+        RaycastHit hit;
+
+        currentDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+        //If the camera is far away enough from the golf ball (so that the camera hopefully doesn't go inside
+        if ( currentDistance > minDistance)
+        {
+            //Try raycasting. If we DONT hit anything (which should be most of the time):
+            if (!Physics.Raycast(transform.position, inverseVeloc, out hit, cameraDistance))
+            {
+
+                //Place the camera at the expected position
+                lerpPosition = transform.position + inverseVeloc * (cameraDistance + .01f);
+
+            }
+            else
+            {   
+                //Otherwise, place the camera at the position of the object we hit so that it doesn't go inside a wall.
+                lerpPosition = hit.point;
+            }
+
+            float lerpDistance = Vector3.Distance(Camera.main.transform.position, lerpPosition);
+
+            Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, lerpPosition, myBody.velocity.magnitude * followPercentage) ;//Vector3.Lerp(Camera.main.transform.position, lerpPosition, .1f);
+
+
+        }
+        else
+        {   
+            //move away from the golf ball
+            Camera.main.transform.position = Vector3.MoveTowards(Camera.main.transform.position, transform.position, -.1f);
+        }
+
+
+
+        
+    }
+}
--- Golf-Assassin/Assets/Scripts/flickerControls.cs
+++ Golf-Assassin/Assets/Scripts/flickerControls.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class flickerControls : MonoBehaviour
 {
     public float powerMod;
+    public RectTransform rectTransform;
+    public Vector2 touch;
 
+    private Vector2 startDragPos, endDragPos;
+    private Vector3 flickDir;
     private Touch touchCtrl;
     private bool grounded;
     private Rigidbody rgbd;
@@ -20,29 +25,75 @@ public class flickerControls : MonoBehaviour
     void Start()
     {
         rgbd = GetComponent<Rigidbody>();
+        //rgbd.freezeRotation = true;
         grounded = true;
+        startDragPos = Vector2.zero;
+        endDragPos = Vector2.zero;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0)
+        //Old Method
+        //if(Input.touchCount > 0)
+        //{
+        //    touchCtrl = Input.GetTouch(0);
+        //    Debug.Log("ADADAD");
+        //    Vector2 start, flickDirection;
+        //    if (touchCtrl.phase == TouchPhase.Began && grounded)
+        //    {
+        //        //detected touch.
+        //        start = touchCtrl.position;
+        //        Debug.Log("Touched");
+        //    }
+
+        //    if(touchCtrl.phase == TouchPhase.Moved)
+        //    {
+
+        //    }
+        //}
+        //Debug.Log(rgbd.velocity.magnitude);
+            if (grounded && rgbd.velocity.magnitude < 3.5 && rgbd.velocity.magnitude > .0001)
+            {
+                rgbd.velocity = Vector3.zero;
+                Vector3 direction = transform.position - Camera.main.transform.position;
+                Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.up);
+                transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, newRotation, .05f);
+                Debug.Log("ADADD");
+            }
+
+    }
+
+    public void OnDragBegin(BaseEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, ((PointerEventData)eventData).position, null, out startDragPos);
+        Debug.Log("BEGIN");
+    }
+
+    public void OnDragEnd(BaseEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, ((PointerEventData)eventData).position, null, out endDragPos);
+        if (grounded)
         {
-            touchCtrl = Input.GetTouch(0);
-            Vector2 start, flickDirection;
-            if (touchCtrl.phase == TouchPhase.Began && grounded)
-            {
-                //detected touch.
-                start = touchCtrl.position;
-                Debug.Log("Touched");
-            }
+            //flickDir = endDragPos - startDragPos;
+            //float angle = angleBar.GetAngle();
+            //flickDir += Vector3.forward;
+            //flickDir *= powerMod;
+            //rgbd.AddForce(flickDir, ForceMode.Impulse);
+            //grounded = false;
+            //Debug.Log("END");
+            //flickDir = Vector3.forward;
+            flickDir = Quaternion.AngleAxis(angleBar.GetAngle(), Vector3.left) * Vector3.forward;
+            Debug.Log(flickDir * (endDragPos - startDragPos).magnitude * powerMod);
+            rgbd.AddForce((flickDir * (endDragPos - startDragPos).magnitude)/5, ForceMode.Impulse);
 
-            if(touchCtrl.phase == TouchPhase.Moved)
-            {
-
-            }
         }
-
+        else
+        {
+            rgbd.AddForce((endDragPos - startDragPos).normalized*powerMod);
+        }
+        
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -52,4 +103,13 @@ public class flickerControls : MonoBehaviour
             grounded = true;
         }
     }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("GroundSurface"))
+        {
+            grounded = false;
+        }
+    }
+
 }
