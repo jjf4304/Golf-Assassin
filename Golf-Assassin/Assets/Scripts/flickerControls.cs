da7613a923a99aa09a902448194ba9fba4ca29c7
Joshua Fredrickson (RIT Student) <jjf4304@ad.rit.edu>
2020-03-02 10:51:52 -0500

    Update flickerControls.cs

--- Golf-Assassin/Assets/Scripts/flickerControls.cs
+++ Golf-Assassin/Assets/Scripts/flickerControls.cs
@@ -9,6 +9,7 @@ public class flickerControls : MonoBehaviour
     public RectTransform rectTransform;
     public Vector2 touch;
 
+    private float timeForFlick;
     private Vector2 startDragPos, endDragPos;
     private Vector3 flickDir;
     private Touch touchCtrl;
@@ -37,38 +38,20 @@ public class flickerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Old Method
-        //if(Input.touchCount > 0)
-        //{
-        //    touchCtrl = Input.GetTouch(0);
-        //    Debug.Log("ADADAD");
-        //    Vector2 start, flickDirection;
-        //    if (touchCtrl.phase == TouchPhase.Began && grounded)
-        //    {
-        //        //detected touch.
-        //        start = touchCtrl.position;
-        //        Debug.Log("Touched");
-        //    }
-
-        //    if(touchCtrl.phase == TouchPhase.Moved)
-        //    {
-
-        //    }
-        //}
-        //Debug.Log(rgbd.velocity.magnitude);
-            if (grounded && rgbd.velocity.magnitude < 3.5 && rgbd.velocity.magnitude > .0001)
-            {
-                rgbd.velocity = Vector3.zero;
-                Vector3 direction = transform.position - Camera.main.transform.position;
-                Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.up);
-                transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, newRotation, .05f);
-                Debug.Log("ADADD");
-            }
+        if (grounded && rgbd.velocity.magnitude < 3.5 && rgbd.velocity.magnitude > .0001)
+        {
+            rgbd.velocity = Vector3.zero;
+            Vector3 direction = transform.position - Camera.main.transform.position;
+            Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, newRotation, .05f);
+            Debug.Log("ADADD");
+        }
 
     }
 
     public void OnDragBegin(BaseEventData eventData)
     {
+        timeForFlick = Time.time;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, ((PointerEventData)eventData).position, null, out startDragPos);
         Debug.Log("BEGIN");
         scoreThing.MinusScore(1);
@@ -79,18 +62,17 @@ public class flickerControls : MonoBehaviour
         RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, ((PointerEventData)eventData).position, null, out endDragPos);
         if (grounded)
         {
-            //flickDir = endDragPos - startDragPos;
-            //float angle = angleBar.GetAngle();
-            //flickDir += Vector3.forward;
-            //flickDir *= powerMod;
-            //rgbd.AddForce(flickDir, ForceMode.Impulse);
-            //grounded = false;
-            //Debug.Log("END");
-            //flickDir = Vector3.forward;
-            Vector3 forward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
-            flickDir = Quaternion.AngleAxis(angleBar.GetAngle(), Vector3.left) * forward;
+            timeForFlick = Time.time - timeForFlick;
 
-            Debug.Log(flickDir * (endDragPos - startDragPos).magnitude * powerMod);
+            Vector2 angleVec = (endDragPos - startDragPos);
+            float angle = angleVec.y / 10;
+            Debug.Log(angle);
+
+            Vector3 forward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
+            flickDir = Quaternion.AngleAxis(angle, Vector3.left) * forward;
+
+            //Debug.Log(flickDir * (endDragPos - startDragPos).magnitude * powerMod);
+            float forceMod = ((endDragPos - startDragPos).magnitude / 5) / timeForFlick;
 
             Vector3 force = flickDir * (endDragPos - startDragPos).magnitude / 5;
             force.y = Mathf.Abs(force.y);
