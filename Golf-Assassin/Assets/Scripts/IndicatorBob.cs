abb882f75d3f6b8882824e520cc037d7a0ea845c
Sam Belisle <spb6892@rit.edu>
2020-03-05 00:24:23 -0500

    Add indicators, fix UI

---
+++ Golf-Assassin/Assets/Scripts/IndicatorBob.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorBob : MonoBehaviour
+{
+    private float max;
+    private float min;
+    private float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        max = gameObject.transform.position.y;
+        min = max - 5;
+        t = 0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(Vector3.up);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, Mathf.Lerp(max, min, t), gameObject.transform.position.z);
+        t += 0.01f;
+        if (t > 1.0f)
+        {
+            float temp = max;
+            max = min;
+            min = temp;
+            t = 0.0f;
+        }
+    }
+}
---
+++ Golf-Assassin/Assets/Scripts/RotateUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateUI : MonoBehaviour
+{
+    private Transform ball;
+
+    void Start()
+    {
+        ball = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ball.Rotate(Vector3.up);
+    }
+}
